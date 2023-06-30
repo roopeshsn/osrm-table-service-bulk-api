1f35559edabbf24a749f4e051ec4fea737996e6f
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2023-06-30 09:12:26 +0530

    Implemented GET request

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,35 @@
-use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
 use std::fmt::Write;
 
 #[derive(Debug)]
-struct Value {
-    distance: f32,
-    duration: f32,
+struct Data {
+    distances: Vec<Vec<f64>>,
+    durations: Vec<Vec<f64>>,
 }
 
-fn random_lat_long_string_generation(lat: f64, long: f64, combinations: usize) -> String {
-    let lat_upto_two = format!("{:.1}", lat);
-    let long_upto_two = format!("{:.1}", long);
-    let mut url = String::from("router.project-osrm.org/table/v1/driving/");
-    let mut last_three = String::from("000");
-    for i in 0..=combinations {
-        let mut pair = String::new();
-        let random_lat = format!("{}{}", lat_upto_two, last_three);
-        let random_long = format!("{}{}", long_upto_two, last_three);
-        if i > 0 {
-            pair.push(';');
-        }
-        pair.push_str(&random_long);
-        pair.push(',');
-        pair.push_str(&random_lat);
-        println!("{}", pair);
-        url.push_str(&pair);
-        if i >= 0 && i <= 9 {
-            last_three = increment_last_three(&last_three, 2, i + 1);
-        }
-        if i >= 10 && i <= 99 {
-            last_three = increment_last_three(&last_three, 1, i + 1);
-        }
-        if i >= 100 && i <= 999 {
-            last_three = increment_last_three(&last_three, 0, i + 1);
-        }
-    }
-    url.push_str("?annotations=distance");
-    url
+#[derive(Debug, Deserialize, Serialize)]
+struct TableResponse {
+    code: String,
+    distances: Vec<Vec<f64>>,
+    destinations: Vec<Destination>,
+    durations: Vec<Vec<f64>>,
+    sources: Vec<Source>,
 }
 
-fn generate_coordinates(lat: f64, long: f64, combinations: usize) -> Vec<[f64; 2]> {
-    let mut res = Vec::new();
-    let lat_upto_two = format!("{:.1}", lat);
-    let long_upto_two = format!("{:.1}", long);
-    let mut last_three = String::from("000");
-    for i in 0..=combinations {
-        let mut pair = [0.0; 2];
-        let random_lat = format!("{}{}", lat_upto_two, last_three);
-        let random_long = format!("{}{}", long_upto_two, last_three);
-        pair[0] = random_lat.parse().unwrap();
-        pair[1] = random_long.parse().unwrap();
-        res.push(pair);
-        if i >= 0 && i <= 9 {
-            last_three = increment_last_three(&last_three, 2, i + 1);
-        }
-        if i >= 10 && i <= 99 {
-            last_three = increment_last_three(&last_three, 1, i + 1);
-        }
-        if i >= 100 && i <= 999 {
-            last_three = increment_last_three(&last_three, 0, i + 1);
-        }
-    }
-    res
+#[derive(Debug, Deserialize, Serialize)]
+struct Destination {
+    hint: String,
+    distance: f64,
+    name: String,
+    location: Vec<f64>,
 }
 
-fn increment_last_three(val: &str, index: usize, replacement: usize) -> String {
-    if replacement >= 0 && replacement <= 9 {
-        let mut new_val = val.to_string();
-        new_val.replace_range(index..index + 1, &replacement.to_string());
-        new_val
-    } else if replacement >= 10 && replacement <= 99 {
-        val[..index].to_string() + &replacement.to_string()
-    } else if replacement >= 100 && replacement <= 999 {
-        replacement.to_string()
-    } else {
-        String::new()
-    }
+#[derive(Debug, Deserialize, Serialize)]
+struct Source {
+    hint: String,
+    distance: f64,
+    name: String,
+    location: Vec<f64>,
 }
 
 fn generate_url_string(coordinates: Vec<[f64; 2]>) -> String {
@@ -86,20 +41,14 @@ fn generate_url_string(coordinates: Vec<[f64; 2]>) -> String {
         if i != 0 {
             url.push(';');
         }
-        write!(
-            &mut url,
-            "{},{}",
-            longitude,
-            latitude
-        )
-        .unwrap();
+        write!(&mut url, "{},{}", longitude, latitude).unwrap();
     }
     url.push_str("?annotations=distance,duration");
     url
 }
 
 fn create_hashmap(locations_list: Vec<[f64; 2]>) {
-    let locations_map: HashMap<String, Value> = HashMap::new();
+    // let locations_map: HashMap<String, Value> = HashMap::new();
     let locations_list_size = locations_list.len();
     for first in (0..locations_list_size).step_by(50) {
         let end_of_first = first + 50;
@@ -117,22 +66,47 @@ fn create_hashmap(locations_list: Vec<[f64; 2]>) {
             };
             let first_list = &locations_list[first..end_of_first];
             let second_list = &locations_list[second..end_of_second];
+
             // Perform operations with first_list and second_list
             // firstChunk and secondChunk are combined in to one list of coordinates
             let mut combined_chunks = Vec::new();
             combined_chunks.extend_from_slice(&first_list);
             combined_chunks.extend_from_slice(&second_list);
             let url = generate_url_string(combined_chunks);
+            println!("{}", url);
+
             // Todo:
             // 1. API call
             // 2. Process the response body
             // 3. Push key value pair to locations_map
+
+            let distances = get_data(url);
+            println!("{:#?}", distances)
         }
     }
 }
 
-fn main() {
-    let locations_list = generate_coordinates(80.0423, 13.3356, 20);
-    create_hashmap(locations_list)
+fn get_data(url: String) -> Result<Data, reqwest::Error> {
+    let resp: TableResponse = reqwest::blocking::get(url)?.json()?;
+    let data = Data {
+        distances: resp.distances,
+        durations: resp.durations,
+    };
+    Ok(data)
 }
 
+fn generate_array_of_coordinates(latitude: f64, longitude: f64, times: i32) -> Vec<[f64; 2]> {
+    let mut array: Vec<[f64; 2]> = vec![];
+    for i in 0..times {
+        let lat = latitude + (i as f64) * 0.001;
+        let lon = longitude + (i as f64) * 0.001;
+        array.push([lon, lat]);
+    }
+
+    array
+}
+
+fn main() {
+    let locations_list = generate_array_of_coordinates(80.0, 13.0, 5);
+    let _ = create_hashmap(locations_list);
+}
